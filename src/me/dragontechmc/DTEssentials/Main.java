009f31dec5506c5a2959694688520a985a5d85fa
Magnusri <magnus_rydjord@hotmail.com>
2016-01-02 00:47:42 +0100

    base

---
+++ src/me/dragontechmc/DTEssentials/Main.java
@@ -0,0 +1,36 @@
+package me.dragontechmc.DTEssentials;
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+
+@Plugin(id = "DTEstl", name = "DTEssentials", version = "1.0")
+public class Main {
+	
+	private Logger logger;
+
+	@Inject
+	private void setLogger(Logger logger) {
+	    this.logger = logger;
+	}
+	
+	public Logger getLogger() {
+	    return logger;
+	}
+    
+	@Listener
+    public void onServerStart(GameStartedServerEvent event) {
+		getLogger().info("DTEssentils has started!");
+    }
+	
+	@Listener
+    public void onServerStop(GameStoppingServerEvent event) {
+		getLogger().info("DTEssentils has stopped!");
+    }
+	
+}
