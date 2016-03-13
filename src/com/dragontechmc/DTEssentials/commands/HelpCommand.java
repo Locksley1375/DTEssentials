62ed35b1ac3fc004014b357ac918c42b93218727
yepidoodles <yepidoodles@live.com>
2016-03-13 20:22:53 +0000

    Update HelpCommand.java

--- src/com/dragontechmc/DTEssentials/commands/HelpCommand.java
+++ src/com/dragontechmc/DTEssentials/commands/HelpCommand.java
@@ -1,59 +1,46 @@
 package com.dragontechmc.DTEssentials.commands;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
-import org.spongepowered.api.command.source.CommandBlockSource;
-import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.service.pagination.PaginationBuilder;
+import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.text.format.TextStyles;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.service.pagination.PaginationService;
-import org.spongepowered.api.text.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.dragontechmc.DTEssentials.DTEssentials;
+
 
 public class HelpCommand implements CommandExecutor {
-
-	DTEssentials plugin = null;
 	
-	public HelpCommand(DTEssentials plugin){
-		this.plugin = plugin;
-	}
+
+	PaginationService paginationService =
+		    Sponge.getServiceManager().provide(PaginationService.class).get();
+		PaginationList.Builder builder = paginationService.builder();
+	
+		
+
+		
 	
 	@Override
 	public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 		
-		String command = args.<String>getOne("command").get();
 		
-		PaginationService paginationService = plugin.getGame().getServiceManager().provide(PaginationService.class).get();
-    	PaginationBuilder builder = paginationService.builder();
 		
-		List<Text> contents = new ArrayList<>();
+		builder.title(Text.of("Dragon Tech Essentials Help"))
+		.contents(Text.of("Item 1"), Text.of("Item 2"), Text.of("Item 3"))
+	    .header(Text.of("Header"))
+	    .footer(Text.of("Footer"))
+	    .padding(Text.of("="));
 		
-		if (command.equals("help") || command.equals("?")){
-    		if(src instanceof Player) {
-    			contents.add(Text.of("/dte help"));
-    			
-    	    	builder.title(Text.of("DTEssentials help"))
-    	        .contents(contents)
-    	        .footer(Text.of(""))
-    	        .paddingString("=")
-    	        .sendTo(src);
-        	}
-        	else if(src instanceof ConsoleSource) {
-        		src.sendMessage(Text.of("-- DTEssentials plugin help --"));
-        		src.sendMessage(Text.of("/dte help"));
-        	}
-        	else if(src instanceof CommandBlockSource) {
-        	    
-        	}
-    	}
-		
-		return CommandResult.success();
+        return CommandResult.success();
+	
 	}
+
 }
