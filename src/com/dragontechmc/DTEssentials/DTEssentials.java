65615a8b99fd47b733159c3e18b65c82b038e5e5
yepidoodles <yepidoodles@live.com>
2016-03-13 20:21:54 +0000

    Update DTEssentials.java

--- src/com/dragontechmc/DTEssentials/DTEssentials.java
+++ src/com/dragontechmc/DTEssentials/DTEssentials.java
@@ -1,23 +1,31 @@
 package com.dragontechmc.DTEssentials;
 
 import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.*;
-import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.text.Text;
+import java.nio.file.Path;
 
-import com.dragontechmc.DTEssentials.commands.HelpCommand;
-import com.dragontechmc.DTEssentials.commands.PermissionCommands;
+import com.dragontechmc.DTEssentials.commands.*;
 import com.google.inject.Inject;
+
 
 import org.slf4j.Logger;
 
-@Plugin(id = "DTEstl", name = "DTEssentials", version = "1.0")
+@Plugin(id = "dte", name = "DTEssentials 1.8.9", version = "1.0.1")
 public class DTEssentials {
+	
+	
+	@Inject
+	@ConfigDir(sharedRoot = false)
+	private Path configDir;
 
 	@Inject
     private Game game;
@@ -37,39 +45,54 @@ public class DTEssentials {
     public void onServerStart(GameStartedServerEvent event) {
 		getLogger().info("DTEssentials has started!");
 		
-		buildCommands();
     }
 	
+    @Listener
+    public void init(GameInitializationEvent event) {
+        createAndRegisterCommands();
+
+    }
+    // Registers commands
+	private void createAndRegisterCommands() {
+		
+		
+		CommandSpec helpCommand = CommandSpec.builder()
+				 .description(Text.of("Help Command"))
+				 .permission("DTEssentials.new.dtehelp")
+				 .arguments(
+						 GenericArguments.onlyOne(GenericArguments.string(Text.of("String"))))
+				 .executor(new HelpCommand())
+				 .build();
+		
+		CommandSpec memberCommand = CommandSpec.builder()
+			    .description(Text.of("Member Command"))
+			    .permission("DTEssentials.member.membercmd")
+			    .executor(new MemberCommand())
+			    .build();
+		
+		
+		CommandSpec wildCommand = CommandSpec.builder()
+				 .description(Text.of("Wild Command"))
+				 .permission("DTEssentials.new.wildcmd")
+				 .executor(new WildCommand())
+				 .build();
+
+		Sponge.getCommandManager().register(this, helpCommand, "dte", "dtehelp");
+		Sponge.getCommandManager().register(this, memberCommand, "member", "rankup", "join");
+		Sponge.getCommandManager().register(this, wildCommand, "wild", "random");	
+		
+	}
+	
+
 	@Listener
     public void onServerStop(GameStoppingServerEvent event) {
 		getLogger().info("DTEssentials has stopped!");
     }
 	
-	public void buildCommands(){
-		CommandSpec helpCommandSpec = CommandSpec.builder()
-			    .description(Text.builder("DTEssentials help command").color(TextColors.GOLD).build())
-			    .permission("DTEssentials.command.help")
-			    .arguments(
-			    		GenericArguments.onlyOne(GenericArguments.string(Text.of("command")))
-			    )
-			    .executor(new HelpCommand(this))
-			    .build();
-
-		CommandSpec permissionCommandSpec = CommandSpec.builder()
-			    .description(Text.builder("DTEssentials help command").color(TextColors.GOLD).build())
-			    .arguments(
-			    		GenericArguments.onlyOne(GenericArguments.player(Text.of("player"))),
-			    		GenericArguments.onlyOne(GenericArguments.string(Text.of("operation"))),
-			    		GenericArguments.onlyOne(GenericArguments.string(Text.of("permission")))
-			    )
-			    .executor(new PermissionCommands(this))
-			    .build();
-		
-		game.getCommandManager().register(this, helpCommandSpec, "dte", "dt");
-		game.getCommandManager().register(this, permissionCommandSpec, "permission", "perm");
-	}
-	
 	public Game getGame() {
         return game;
     }
+
+	
+	
 }
